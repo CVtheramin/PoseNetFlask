88e2fb802f5b2f2a0c1c0e2bc7c49950d415f226
meredith <meredith@paom.com>
2020-08-27 21:25:17 -0400

    svg

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # PoseNetFlask
 the flask app running a basic PoseNet sensor. Primarily used to figure out how PoseNet works
+
+# Run the Flask Appp
+FLASK_APP=backend flask run
--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
+from werkzeug.utils import secure_filename
 
 
 def create_app():
     app = Flask(__name__)
 
+    def render_svg(name):
+        svg = file('static/svg/+'+name+'.svg').read()
+        return render_template('index.html',{'svg':svg})
+
     @app.route('/')
-    def sample():
-        return render_template('practice.html')
+    def index():
+        return render_svg('index')
+
+    @app.route('/about')
+    def about():
+        return render_svg('about')
+
+    @app.route('/play')
+    def play():
+        return render_svg('play')
+
+    @app.route('/remix')
+    def remix():
+        return render_svg('remix')
+
+    @app.route('/dance')
+    def dance():
+        return render_svg('dance')
 
     return app
